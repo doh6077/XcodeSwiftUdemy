41e87488f10ba83ae353abfbd49a49a056011786
dohee <kimdohee@sheridancollege.ca>
2025-03-03 18:26:56 -0500

    Add variables and IBaction for button

--- Week1Calculator/ViewController.swift
+++ Week1Calculator/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    var numberOnScreen: Double = 0.0
+    
+    var previousNumber: Double = 0.0
+    
+    var performingMath = false
+    
+    var operation = 0
+    
+    var btnSound: AVAudioPlayer!
+    
+    
+    //MARK: -IBOutlets
+    @IBOutlet var outputLbl: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func numberBtn(_ sender: UIButton){
+        //When we press a number on the calculator
+        
+    }
+    
+    @IBAction func operators(_ sender: UIButton){
+        // when we press an operator on the calculator
+        
     }
 
 
