4009166484280026d5d84482f601bd6de2bffd92
dohee <kimdohee@sheridancollege.ca>
2025-03-03 19:36:05 -0500

    Add label and IBOutlet for each textfield

--- MultipageApp/ViewController.swift
+++ MultipageApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    
+    @IBOutlet weak var gender: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
